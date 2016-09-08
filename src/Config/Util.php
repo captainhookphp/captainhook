e89e6f6a3ca97fb5c6a70340c8d0ba8516179a0f
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2016-09-08 20:17:46 +0200

    Add missing 'static' keyword to 'keysExist' method

--- src/Config/Util.php
+++ src/Config/Util.php
@@ -80,7 +80,7 @@ abstract class Util
      * @param  array $subject
      * @return bool
      */
-    private function keysExist(array $keys, array $subject)
+    private static function keysExist(array $keys, array $subject)
     {
         foreach ($keys as $key) {
             if (!isset($subject[$key])) {
