1c678499b9d1505cbb121b7c7a1e98f6f2efa3a5
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2016-09-12 16:06:59 +0200

    Add Util class to compute sub directory relations
    This is mostly to allow "non-default" repository structures like this
    
         - project/someDir/composer.json
         - project/someDir/captainhook.json
         - project/someDir/vendor/*
    
     So if you use the '--git-directory' option to specify your git directory
     the created hook scripts contain the correct paths.

---
+++ src/Storage/Util.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * This file is part of CaptainHook.
+ *
+ * (c) Sebastian Feldmann <sf@sebastian.feldmann.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace sebastianfeldmann\CaptainHook\Storage;
+
+/**
+ * Util class
+ *
+ * @package CaptainHook
+ * @author  Sebastian Feldmann <sf@sebastian-feldmann.info>
+ * @link    https://github.com/sebastianfeldmann/captainhook
+ * @since   Class available since Release 1.0.4
+ */
+class Util
+{
+    /**
+     * Array representation of a path.
+     *
+     * @param  string $path
+     * @return array
+     */
+    public static function pathToArray($path)
+    {
+        return explode(DIRECTORY_SEPARATOR, ltrim($path, DIRECTORY_SEPARATOR));
+    }
+
+    /**
+     * Is the given subDir a sub directory of given parentDir.
+     *
+     * @param  array $subDir
+     * @param  array $parentDir
+     * @return bool
+     */
+    public static function isSubDirectoryOf(array $subDir, array $parentDir)
+    {
+        foreach ($parentDir as $index => $dir) {
+            if (!isset($subDir[$index]) || $dir !== $subDir[$index]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Return the relative path from parentDir to subDir.
+     *
+     * @param  array $subDir
+     * @param  array $parentDir
+     * @return string
+     */
+    public static function getSubPathOf(array $subDir, array $parentDir)
+    {
+        if (!self::isSubDirectoryOf($subDir, $parentDir)) {
+            throw new \RuntimeException('Invalid sub directory');
+        }
+
+        $path = [];
+        foreach (array_slice($subDir, count($parentDir)) as $dir) {
+            $path[] = $dir;
+        }
+        return implode(DIRECTORY_SEPARATOR, $path);
+    }
+}
---
+++ tests/CaptainHook/Storage/UtilTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of CaptainHook.
+ *
+ * (c) Sebastian Feldmann <sf@sebastian.feldmann.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace sebastianfeldmann\CaptainHook\Storage;
+
+class UtilTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests Util::pathToArray
+     */
+    public function testAbsolutePathToArray()
+    {
+        $path = Util::pathToArray('/foo/bar/baz');
+
+        $this->assertEquals(3, count($path));
+        $this->assertEquals('bar', $path[1]);
+    }
+
+    /**
+     * Tests Util::pathToArray
+     */
+    public function testRelativePathToArray()
+    {
+        $path = Util::pathToArray('foo/bar/baz');
+
+        $this->assertEquals(3, count($path));
+        $this->assertEquals('bar', $path[1]);
+    }
+
+    /**
+     * Tests Util::isSubDirectoryOf
+     */
+    public function testIsSubdirectory()
+    {
+        $this->assertTrue(Util::isSubDirectoryOf(Util::pathToArray('/foo/bar/baz'), Util::pathToArray('/foo/bar')));
+        $this->assertTrue(Util::isSubDirectoryOf(Util::pathToArray('/foo/bar'), Util::pathToArray('/foo')));
+        $this->assertFalse(Util::isSubDirectoryOf(Util::pathToArray('/foo/bar/baz'), Util::pathToArray('/fiz/baz')));
+        $this->assertFalse(Util::isSubDirectoryOf(Util::pathToArray('/foo'), Util::pathToArray('/bar')));
+    }
+}
