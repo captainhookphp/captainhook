697e487991d9c1e716c4db252a671340c035f332
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2018-01-14 19:57:40 +0100

    Prepare version 2.0.5

--- src/CH.php
+++ src/CH.php
@@ -19,6 +19,6 @@ namespace SebastianFeldmann\CaptainHook;
  */
 class CH
 {
-    const VERSION      = '2.0.3';
-    const RELEASE_DATE = '2017/04/25';
+    const VERSION      = '2.0.5';
+    const RELEASE_DATE = '2018/01/14';
 }
