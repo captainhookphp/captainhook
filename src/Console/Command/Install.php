084a1896d5a6b2d317ace27f72be32a7bf6a1d1a
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2018-08-24 19:28:15 +0200

    Add throws to PHPDoc

--- src/Console/Command/Install.php
+++ src/Console/Command/Install.php
@@ -57,6 +57,7 @@ class Install extends Base
      * @param  \Symfony\Component\Console\Input\InputInterface   $input
      * @param  \Symfony\Component\Console\Output\OutputInterface $output
      * @return void
+     * @throws \SebastianFeldmann\CaptainHook\Exception\InvalidHookName
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
