0facfbfdf60d79c0cd53fc178e20e602d1ec3d28
Andreas Frömer <icanhazstring@users.noreply.github.com>
2019-05-09 18:39:04 +0200

    Readd deleted test

---
+++ tests/CaptainHook/Console/Application/SetupTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of CaptainHook.
+ *
+ * (c) Sebastian Feldmann <sf@sebastian.feldmann.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace CaptainHook\App\Console\Application;
+
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+use PHPUnit\Framework\TestCase;
+
+class SetupTest extends TestCase
+{
+    public function testRun()
+    {
+        $output = new NullOutput();
+        $input  = new ArrayInput([
+            'command'         => 'run',
+            'hook'            => 'pre-push',
+            '--configuration' => CH_PATH_FILES . DIRECTORY_SEPARATOR . 'config/valid.json'
+        ]);
+        $app = new Setup();
+        $app->setAutoExit(false);
+        $app->run($input, $output);
+
+        $this->assertTrue(true);
+    }
+}
