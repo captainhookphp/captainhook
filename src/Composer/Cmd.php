1a64d0390b0a62f1eba3b79c68cb4b10538acbbe
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2019-06-18 01:02:01 +0200

    Make sure install uses the 'force' option

--- src/Composer/Cmd.php
+++ src/Composer/Cmd.php
@@ -79,7 +79,7 @@ abstract class Cmd
             '--git-directory' => self::extract(CH::COMPOSER_GIT_DIR, $extra, '.git'),
             '--run-mode'      => self::extract(CH::COMPOSER_RUN_MODE, $extra, 'local'),
             '--container'     => self::extract(CH::COMPOSER_CONTAINER, $extra, ''),
-            '-f'              => '-f'
+            '-f'              => true
         ];
         $app->run(new ArrayInput($options));
     }
