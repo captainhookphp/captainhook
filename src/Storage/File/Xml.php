8114f51ba62c95e76ead48fa0fadcda4f47f114c
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2019-09-24 12:51:53 +0200

    Fix psr12 whitespace issue

--- src/Storage/File/Xml.php
+++ src/Storage/File/Xml.php
@@ -34,7 +34,7 @@ class Xml extends File
         libxml_use_internal_errors($old);
 
         if (count($errors) || $xml === false) {
-            throw new \RuntimeException('xml file \''. $this->path . '\': ' . $errors[0]->message);
+            throw new \RuntimeException('xml file \'' . $this->path . '\': ' . $errors[0]->message);
         }
         return $xml;
     }
