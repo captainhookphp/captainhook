72dedb9f89ede87e32b95c63b0843b4217715bfe
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2020-01-13 13:08:21 +0100

    Add staged files of type condition

---
+++ src/Hook/Condition/FileStaged/OfType.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * This file is part of CaptainHook
+ *
+ * (c) Sebastian Feldmann <sf@sebastian.feldmann.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CaptainHook\App\Hook\Condition\FileStaged;
+
+use CaptainHook\App\Console\IO;
+use CaptainHook\App\Hook\Condition;
+use SebastianFeldmann\Git\Repository;
+
+/**
+ * Class OfType
+ *
+ * All FileStaged conditions are only applicable for `pre-commit` hooks.
+ *
+ * Example configuration:
+ *
+ * "action": "composer install"
+ * "conditions": [
+ *   {"exec": "\\CaptainHook\\App\\Hook\\Condition\\FileStaged\\OfType",
+ *    "args": [
+ *      "php"
+ *    ]}
+ * ]
+ *
+ * @package CaptainHook
+ * @author  Sebastian Feldmann <sf@sebastian-feldmann.info>
+ * @link    https://github.com/captainhookphp/captainhook
+ * @since   Class available since Release 5.0.0
+ */
+class OfType implements Condition
+{
+    /**
+     * File type to check e.g. 'php' or 'html'
+     *
+     * @var string
+     */
+    private $suffix;
+
+    /**
+     * OfType constructor
+     *
+     * @param string $type
+     */
+    public function __construct(string $type)
+    {
+        $this->suffix = $type;
+    }
+
+    /**
+     * Evaluates the condition
+     *
+     * @param  \CaptainHook\App\Console\IO       $io
+     * @param  \SebastianFeldmann\Git\Repository $repository
+     * @return bool
+     */
+    public function isTrue(IO $io, Repository $repository): bool
+    {
+        $files = $repository->getIndexOperator()->getStagedFilesOfType($this->suffix);
+        return count($files) > 0;
+    }
+}
---
+++ tests/CaptainHook/Hook/Condition/FileStaged/OfTypeTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of CaptainHook.
+ *
+ * (c) Sebastian Feldmann <sf@sebastian-feldmann.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CaptainHook\App\Hook\Condition\FileStaged;
+
+use PHPUnit\Framework\TestCase;
+use CaptainHook\App\Mockery as AppMockery;
+use CaptainHook\App\Console\IO\Mockery as IOMockery;
+
+class OfTypeTest extends TestCase
+{
+    use AppMockery;
+    use IOMockery;
+
+    /**
+     * Tests OfType::isTrue
+     */
+    public function testStagedTrue(): void
+    {
+        $io    = $this->createIOMock();
+        $repo  = $this->createRepositoryMock();
+        $index = $this->createGitIndexOperator();
+        $index->expects($this->once())->method('getStagedFilesOfType')->willReturn(['foo.php', 'bar.php']);
+        $repo->expects($this->once())->method('getIndexOperator')->willReturn($index);
+
+        $ofType = new OfType('php');
+        $this->assertTrue($ofType->isTrue($io, $repo));
+    }
+
+    /**
+     * Tests OfType:isTrue
+     */
+    public function testStagedFalse(): void
+    {
+        $io    = $this->createIOMock();
+        $repo  = $this->createRepositoryMock();
+        $index = $this->createGitIndexOperator();
+        $index->expects($this->once())->method('getStagedFilesOfType')->willReturn([]);
+        $repo->expects($this->once())->method('getIndexOperator')->willReturn($index);
+
+        $ofType = new OfType('js');
+        $this->assertFalse($ofType->isTrue($io, $repo));
+    }
+}
