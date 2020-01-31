d210a10e3e7d41300f55cc0c9827a53b983af901
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2020-01-31 12:15:52 +0100

    Fix file header email address

--- src/Console/IO.php
+++ src/Console/IO.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of CaptainHook
  *
- * (c) Sebastian Feldmann <sf@sebastian.feldmann.info>
+ * (c) Sebastian Feldmann <sf@sebastian-feldmann.info>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,7 +17,7 @@ namespace CaptainHook\App\Console;
  * @package CaptainHook
  * @author  Nils Adermann <naderman@naderman.de>
  * @author  Jordi Boggiano <j.boggiano@seld.be>
- * @author  Sebastian Feldmann <sf@sebastian.feldmann.info>
+ * @author  Sebastian Feldmann <sf@sebastian-feldmann.info>
  * @link    https://github.com/captainhookphp/captainhook
  * @since   Class available since Release 0.9.0
  */
