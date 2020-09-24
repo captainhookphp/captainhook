de7969ceb95ab41f72ecb1847a617401b5348bd7
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2020-09-24 18:25:50 +0200

    Add post-rewrite test case

---
+++ tests/CaptainHook/Runner/Hook/PostRewriteTest.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * This file is part of CaptainHook
+ *
+ * (c) Sebastian Feldmann <sf@sebastian-feldmann.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CaptainHook\App\Runner\Hook;
+
+use CaptainHook\App\Config\Mockery as ConfigMockery;
+use CaptainHook\App\Console\IO\Mockery as IOMockery;
+use CaptainHook\App\Mockery as CHMockery;
+use PHPUnit\Framework\TestCase;
+
+class PostRewriteTest extends TestCase
+{
+    use ConfigMockery;
+    use IOMockery;
+    use CHMockery;
+
+    /**
+     * Tests PrePush::run
+     *
+     * @throws \Exception
+     */
+    public function testRunHookEnabled(): void
+    {
+        $io           = $this->createIOMock();
+        $config       = $this->createConfigMock();
+        $repo         = $this->createRepositoryMock();
+        $hookConfig   = $this->createHookConfigMock();
+
+        // configure the actually called hook
+        $actionConfig = $this->createActionConfigMock();
+        $hookConfig->expects($this->atLeastOnce())->method('getName')->willReturn('post-rewrite');
+        $hookConfig->expects($this->once())->method('isEnabled')->willReturn(true);
+        $hookConfig->expects($this->once())->method('getActions')->willReturn([$actionConfig]);
+
+        // configure the virtual hook
+        $vHookConfig   = $this->createHookConfigMock();
+        $vActionConfig = $this->createActionConfigMock();
+        $vHookConfig->expects($this->once())->method('isEnabled')->willReturn(true);
+        $vHookConfig->expects($this->once())->method('getActions')->willReturn([$vActionConfig]);
+
+        // the config wll return the actually called config and then the virtual hook config
+        $config->expects($this->exactly(2))
+               ->method('getHookConfig')
+               ->willReturnOnConsecutiveCalls($hookConfig, $vHookConfig);
+
+        $io->expects($this->atLeast(1))->method('write');
+
+        $runner = new PostRewrite($io, $config, $repo);
+        $runner->run();
+    }
+
+    /**
+     * Tests PrePush::run
+     *
+     * @throws \Exception
+     */
+    public function testRunVirtualHookDisabled(): void
+    {
+        $io           = $this->createIOMock();
+        $config       = $this->createConfigMock();
+        $repo         = $this->createRepositoryMock();
+        $hookConfig   = $this->createHookConfigMock();
+
+        // configure the actually called hook
+        $actionConfig = $this->createActionConfigMock();
+        $hookConfig->expects($this->atLeastOnce())->method('getName')->willReturn('post-rewrite');
+        $hookConfig->expects($this->once())->method('isEnabled')->willReturn(true);
+        $hookConfig->expects($this->once())->method('getActions')->willReturn([$actionConfig]);
+
+        // configure the virtual hook
+        $vHookConfig = $this->createHookConfigMock();
+        $vHookConfig->expects($this->once())->method('isEnabled')->willReturn(false);
+
+        // the config wll return the actually called config and then the virtual hook config
+        $config->expects($this->exactly(2))
+               ->method('getHookConfig')
+               ->willReturnOnConsecutiveCalls($hookConfig, $vHookConfig);
+
+        $io->expects($this->atLeast(1))->method('write');
+
+        $runner = new PostRewrite($io, $config, $repo);
+        $runner->run();
+    }
+}
