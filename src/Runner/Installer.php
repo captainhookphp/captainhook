3e18ea988c380ba98271a873e6ed0be5acae24da
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2021-10-23 16:28:21 +0200

    Add default value indication
    As mentioned in issue #153 the default value was not communicated
    clearly. So the default value was changed to upper case.

--- src/Runner/Installer.php
+++ src/Runner/Installer.php
@@ -199,7 +199,7 @@ class Installer extends RepositoryAware
 
         $doIt = true;
         if ($ask) {
-            $answer = $this->io->ask('  <info>Install \'' . $hook . '\' hook?</info> <comment>[y,n]</comment> ', 'y');
+            $answer = $this->io->ask('  <info>Install \'' . $hook . '\' hook?</info> <comment>[Y,n]</comment> ', 'y');
             $doIt   = IOUtil::answerToBool($answer);
         }
 
