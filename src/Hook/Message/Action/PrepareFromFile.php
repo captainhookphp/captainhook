23d2636d7065c1bf8a23183fc4be41956a924ce9
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2022-10-23 12:27:01 +0200

    Add prepare from file action
    This actions reads the commit message from a file.
    For example from a file that was written by the 'CacheOnFail'
    action to reuse commit messages from failed commits.

---
+++ src/Hook/Message/Action/PrepareFromFile.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * This file is part of CaptainHook
+ *
+ * (c) Sebastian Feldmann <sf@sebastian-feldmann.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace CaptainHook\App\Hook\Message\Action;
+
+use CaptainHook\App\Config;
+use CaptainHook\App\Console\IO;
+use CaptainHook\App\Exception\ActionFailed;
+use CaptainHook\App\Hook\Action;
+use SebastianFeldmann\Git\CommitMessage;
+use SebastianFeldmann\Git\Repository;
+
+/**
+ * Class PrepareFromFile
+ *
+ * @package CaptainHook
+ * @author  Sebastian Feldmann <sf@sebastian-feldmann.info>
+ * @link    https://github.com/captainhookphp/captainhook
+ * @since   Class available since Release 6.0.0
+ */
+class PrepareFromFile implements Action
+{
+    /**
+     * Execute the configured action
+     *
+     * @param  \CaptainHook\App\Config           $config
+     * @param  \CaptainHook\App\Console\IO       $io
+     * @param  \SebastianFeldmann\Git\Repository $repository
+     * @param  \CaptainHook\App\Config\Action    $action
+     * @return void
+     * @throws \Exception
+     */
+    public function execute(Config $config, IO $io, Repository $repository, Config\Action $action): void
+    {
+        $options   = $action->getOptions();
+        $cacheFile = $repository->getRoot() . '/' . $options->get('file', '');
+        if (null === $options->get('file')) {
+            throw new ActionFailed('PrepareFromFile requires \'file\' option');
+        }
+        if (!is_file($cacheFile)) {
+            return;
+        }
+        $msg = (string) file_get_contents($cacheFile);
+        $repository->setCommitMsg(
+            new CommitMessage($msg, $repository->getCommitMsg()->getCommentCharacter())
+        );
+        if (!$options->get('keep', false)) {
+            unlink($cacheFile);
+        }
+    }
+}
