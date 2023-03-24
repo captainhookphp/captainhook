f5153277c3dd81b6c9b4f899041af153681ae1de
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2023-03-24 10:39:57 +0100

    Add new file condition
    By  combining condition with 'AND' or 'OR' you can not make sure that a
    single file matches all the conditions. The new condition makes sure
    a single file is matching all conditions.
    
    Available options:
     - inDirectory
     - ofType

---
+++ src/Hook/Condition/FileStaged/ThatIs.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * This file is part of CaptainHook
+ *
+ * (c) Sebastian Feldmann <sf@sebastian-feldmann.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CaptainHook\App\Hook\Condition\FileStaged;
+
+use CaptainHook\App\Console\IO;
+use CaptainHook\App\Hook\Condition;
+use CaptainHook\App\Hook\Constrained;
+use CaptainHook\App\Hook\Restriction;
+use CaptainHook\App\Hooks;
+use SebastianFeldmann\Git\Repository;
+
+/**
+ * Class ThatIs
+ *
+ * All FileStaged conditions are only applicable for `pre-commit` hooks.
+ *
+ * Example configuration:
+ *
+ * "action": "some-action"
+ * "conditions": [
+ *   {"exec": "\\CaptainHook\\App\\Hook\\Condition\\FileStaged\\ThatIs",
+ *    "args": [
+ *      {"ofType": "php", "inDirectory": "foo/"}
+ *    ]}
+ * ]
+ *
+ * @package CaptainHook
+ * @author  Sebastian Feldmann <sf@sebastian-feldmann.info>
+ * @link    https://github.com/captainhookphp/captainhook
+ * @since   Class available since Release 5.16.0
+ */
+class ThatIs implements Condition, Constrained
+{
+    /**
+     * Directory path to check e.g. 'src/' or 'path/To/Custom/Directory/'
+     *
+     * @var string
+     */
+    private string $directory;
+
+    /**
+     * File type to check e.g. 'php' or 'html'
+     *
+     * @var string
+     */
+    private string $suffix;
+
+    /**
+     * OfType constructor
+     *
+     * @param array<string, string> $options
+     */
+    public function __construct(array $options)
+    {
+        $this->directory = $options['inDirectory'] ?? '';
+        $this->suffix    = $options['ofType'] ?? '';
+    }
+
+    /**
+     * Return the hook restriction information
+     *
+     * @return \CaptainHook\App\Hook\Restriction
+     */
+    public static function getRestriction(): Restriction
+    {
+        return Restriction::fromArray([Hooks::PRE_COMMIT]);
+    }
+
+    /**
+     * Evaluates the condition
+     *
+     * @param  \CaptainHook\App\Console\IO       $io
+     * @param  \SebastianFeldmann\Git\Repository $repository
+     * @return bool
+     */
+    public function isTrue(IO $io, Repository $repository): bool
+    {
+        $files = $repository->getIndexOperator()->getStagedFiles();
+        $files = $this->filterFilesByDirectory($files);
+        $files = $this->filterFilesByType($files);
+        return count($files) > 0;
+    }
+
+    /**
+     * Remove all files not in a given directory
+     *
+     * @param  array<string> $files
+     * @return array<string>
+     */
+    private function filterFilesByDirectory(array $files): array
+    {
+        if (empty($this->directory)) {
+            return $files;
+        }
+        return array_filter($files, fn($file) => strpos($file, $this->directory) !== false);
+    }
+
+    /**
+     * Remove all files not of a configured type
+     *
+     * @param  array<string> $files
+     * @return array<string>
+     */
+    private function filterFilesByType(array $files): array
+    {
+        if (empty($this->suffix)) {
+            return $files;
+        }
+        return array_filter($files, fn($file) => strtolower(pathinfo($file, PATHINFO_EXTENSION)) === $this->suffix);
+    }
+}
---
+++ tests/unit/Hook/Condition/FileStaged/ThatIsTest.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * This file is part of CaptainHook.
+ *
+ * (c) Sebastian Feldmann <sf@sebastian-feldmann.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CaptainHook\App\Hook\Condition\FileStaged;
+
+use PHPUnit\Framework\TestCase;
+use CaptainHook\App\Mockery as AppMockery;
+use CaptainHook\App\Console\IO\Mockery as IOMockery;
+
+class ThatIsTest extends TestCase
+{
+    use AppMockery;
+    use IOMockery;
+
+    /**
+     * Tests ThatIs::getRestriction
+     */
+    public function testPreCommitRestriction(): void
+    {
+        $this->assertTrue(ThatIs::getRestriction()->isApplicableFor('pre-commit'));
+        $this->assertFalse(ThatIs::getRestriction()->isApplicableFor('pre-push'));
+    }
+
+    /**
+     * Tests ThatIs::isTrue
+     */
+    public function testStagedTrueType(): void
+    {
+        $io    = $this->createIOMock();
+        $repo  = $this->createRepositoryMock();
+        $index = $this->createGitIndexOperator(['foo.php', 'bar.php']);
+        $repo->expects($this->once())->method('getIndexOperator')->willReturn($index);
+
+        $thatIs = new ThatIs(['ofType' => 'php']);
+        $this->assertTrue($thatIs->isTrue($io, $repo));
+    }
+
+    /**
+     * Tests ThatIs::isTrue
+     */
+    public function testStagedTrueDirectory(): void
+    {
+        $io    = $this->createIOMock();
+        $repo  = $this->createRepositoryMock();
+        $index = $this->createGitIndexOperator(['foo/foo.php', 'bar/bar.js', 'fiz/baz.txt']);
+        $repo->expects($this->once())->method('getIndexOperator')->willReturn($index);
+
+        $thatIs = new ThatIs(['inDirectory' => 'bar/']);
+        $this->assertTrue($thatIs->isTrue($io, $repo));
+    }
+
+    /**
+     * Tests ThatIs::isTrue
+     */
+    public function testStagedFalseDirectoryAndType(): void
+    {
+        $io    = $this->createIOMock();
+        $repo  = $this->createRepositoryMock();
+        $index = $this->createGitIndexOperator(['foo/foo.php', 'bar/bar.js']);
+        $repo->expects($this->once())->method('getIndexOperator')->willReturn($index);
+
+        $thatIs = new ThatIs(['inDirectory' => 'bar/', 'ofType' => 'php']);
+        $this->assertFalse($thatIs->isTrue($io, $repo));
+    }
+
+    /**
+     * Tests ThatIs:isTrue
+     */
+    public function testStagedFalse(): void
+    {
+        $io    = $this->createIOMock();
+        $repo  = $this->createRepositoryMock();
+        $index = $this->createGitIndexOperator(['foo.php']);
+        $repo->expects($this->once())->method('getIndexOperator')->willReturn($index);
+
+        $thatIs = new ThatIs(['ofType' => 'js']);
+        $this->assertFalse($thatIs->isTrue($io, $repo));
+    }
+}
