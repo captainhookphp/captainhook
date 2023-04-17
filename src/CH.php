524c8660551bafe9c7211440a71a35984e8dfc4b
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2023-04-17 21:48:47 +0200

    Prepare version 5.16.4

--- src/CH.php
+++ src/CH.php
@@ -23,7 +23,7 @@ namespace CaptainHook\App;
  */
 final class CH
 {
-    public const VERSION      = '5.16.3';
-    public const RELEASE_DATE = '2023-03-30';
+    public const VERSION      = '5.16.4';
+    public const RELEASE_DATE = '2023-04-17';
     public const CONFIG       = 'captainhook.json';
 }
