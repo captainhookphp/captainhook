12ec2b4aa5159608ab455168afeced6f7ce5ef45
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2023-06-06 17:25:11 +0200

    Remove SebastianFeldmann namespace from whitelist

--- build/scoper.inc.php
+++ build/scoper.inc.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 return [
     'prefix'    => 'CaptainHook\\Ext',
     'whitelist' => [
-        'CaptainHook\*',
-        'SebastianFeldmann\*'
+        'CaptainHook\*'
     ],
 ];
