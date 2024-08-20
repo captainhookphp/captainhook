fe4c5b63a27ca87c50fa6ffe8e6710c9584ec9f5
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2024-08-20 11:25:19 +0200

    Try to fix PHAR autoloading issue
    The PHAR autoloader does not load the Symfony polyfill bootstrap files.
    That's why the PHAR is crashing if not intl extension is loaded.
    
     - Updated to latest Box version
     - Copied a hack from PHPStand

--- src/CH.php
+++ src/CH.php
@@ -26,7 +26,7 @@ final class CH
     /**
      * Current CaptainHook version
      */
-    public const VERSION = '5.23.3';
+    public const VERSION = '5.23.4-alpha-1';
 
     /**
      * Release date of the current version
